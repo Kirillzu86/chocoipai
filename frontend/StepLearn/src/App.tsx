3c6841def6f51abfcb900b02683e7e224c772e56
Maggich <mvahaevich@mail.ru>
2025-12-12 14:25:54 +0500

    start

---
+++ frontend/StepLearn/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Творим</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
